2eac69661d86eeec2c214d7fb17b48f78dd12da2
hallgato <hallgato@LABOR210-07>
2022-03-29 14:23:16 +0200

    Display rész, közepe nincs meg

--- Tictactoe/MainWindow.xaml.cs
+++ Tictactoe/MainWindow.xaml.cs
@@ -24,5 +24,56 @@ namespace Tictactoe
         {
             InitializeComponent();
         }
+
+        private void Window_SizeChanged(object sender, SizeChangedEventArgs e)
+        {
+            if (grid.ActualWidth > 0 && grid.ActualHeight > 0)
+            {
+                display.Resize(new Size()
+                {
+                    Width = ActualWidth,
+                    Height = ActualHeight
+                });
+            }
+        }
+        
+
+
+        private void display_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            if (e.Source.ToString() == "Tictactoe.Display")
+            {
+                int cellnumberx = 0;
+                int cellnumbery = 0;
+                int x = (int)e.GetPosition(grid).X;
+                int y = (int)e.GetPosition(grid).Y;
+
+                cellnumberx = x / (int)(display.ActualWidth / 3);
+                cellnumbery = y / (int)(display.ActualHeight / 3);
+
+               
+
+                int midx = ((int)(display.ActualHeight / 3) / 2) * (cellnumberx + 1);
+                int midy = ((int)(display.ActualWidth / 3) / 2) * (cellnumbery + 1);
+
+                Line myLine = new Line();
+                myLine.Stroke = System.Windows.Media.Brushes.LightSteelBlue;
+                myLine.X1 = midx;
+                myLine.X2 = 0;
+                myLine.Y1 = midy;
+                myLine.Y2 = 0;
+                myLine.HorizontalAlignment = HorizontalAlignment.Left;
+                myLine.VerticalAlignment = VerticalAlignment.Center;
+                myLine.StrokeThickness = 2;
+                grid.Children.Add(myLine);
+            }
+
+
+
+        }
+
+
+
+
     }
 }
